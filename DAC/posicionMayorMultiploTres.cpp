7e13a0146be441264cf9f60e9f47f314cd08c175
soledadlinares <sollinar@gmail.com>
2023-01-06 19:27:57 -0300

    ejercicio de parcial multiplos de 3

---
+++ DAC/posicionMayorMultiploTres.cpp
@@ -0,0 +1,39 @@
+#include <climits>
+#include <string>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Algoritmo que reciba un array y retorne la posicion del mayor elemento multiplo de 3.
+//Si no hay multiplos de 3 se retorna -1
+//Si el mayor multiplo se repite se podra retornar la posicion de cualquiera de sus ocurrencias.
+//SE DEBE DIVIDIR EL PROBLEMA DE FORMA BALANCEADA.
+
+bool esMultiploDeTres(int a){
+    return a % 3 == 0;
+}
+
+int pisicionDelMayorMultiploDeTres(int arr [], int inicio, int fin){
+    if(inicio == fin)
+    {
+        if(esMultiploDeTres(arr[inicio])) return inicio;
+        else return -1;
+    }
+    
+    int mitad = (inicio + fin)/2;
+
+    int posIzq = pisicionDelMayorMultiploDeTres(arr, inicio, mitad);
+    int posDer = pisicionDelMayorMultiploDeTres(arr, mitad + 1, fin);
+
+    if (posIzq == -1) return posDer;
+    if (posDer == -1) return posIzq;
+    if (arr[posIzq] > arr[posDer]) return posIzq;
+    else return posDer;
+}
+
+
+int main (){
+    int valores [] = {3, -15, 100, 27, 49, 9, 27, 200, 8};
+
+    return 0;
+}
