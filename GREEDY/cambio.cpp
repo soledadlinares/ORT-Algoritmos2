24aff451e249bfa1c3dd9b509bbf43d77d68593e
soledadlinares <sollinar@gmail.com>
2023-01-06 21:17:59 -0300

    cambio con disponibilidad de billetes

--- GREEDY/cambio.cpp
+++ GREEDY/cambio.cpp
@@ -26,9 +26,35 @@ void cambio (int billetes [], int cantBilletes, int costo, int pago) {
     }
 }
 
+void cambioConDisponibilidad (int billetes [], int disponibles [], int cantBilletes, int costo, int pago) {
+
+    int entregar = pago - costo;
+    
+    if (entregar <= 0) cout << "No se retorna cambio" << endl;
+    else {
+        for (int j = 0; j < cantBilletes; j++){
+            int billete = billetes[j];
+            int cantidad = entregar/billete;
+
+            if (cantidad > disponibles[j]){
+                cantidad = disponibles[j];
+                entregar -= cantidad * disponibles[j];
+            } else {
+                entregar = entregar % billete;
+            }
+
+            if (cantidad != 0) cout << "Se entregan " << cantidad << " billetes de " << billete << endl;
+        }
+    }
+}
+
 int main (){
     int billetes [] = {2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
-    cambio(billetes, 11, 70, 2020);
+    int disponibles [] = {4, 0, 6, 10, 7, 1, 14, 10, 0, 7, 1};
+    cout << "Cambio infinito" << endl;
+    cambio(billetes, 11, 23, 10023);
+    cout << "Cambio con disponibilidad"<< endl;
+    cambioConDisponibilidad(billetes, disponibles, 11, 23, 10023);
     
     return 0;
 }
