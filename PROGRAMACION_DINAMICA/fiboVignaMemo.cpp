db958e452c434d13ba4a358d11125e0c45ace1f4
soledadlinares <sollinar@gmail.com>
2023-01-07 07:46:36 -0300

    agrego matriz de ya calculado

--- PROGRAMACION_DINAMICA/fiboVignaMemo.cpp
+++ PROGRAMACION_DINAMICA/fiboVignaMemo.cpp
@@ -4,24 +4,27 @@
 #include <cmath>
 using namespace std;
 
-long fiboVignaMemo(int n, int m, long ** memo){
+long fiboVignaMemo(int n, int m, long ** memo, bool ** yaCalculado){
     if(n == 0 || m == 0) return 3;
     if (n + m == 3) return 8;
-    if (memo[n][m] == -1){
-        memo[n][m] = fiboVignaMemo(n, m-1, memo) + fiboVignaMemo(n - 1, m, memo) + fiboVignaMemo(n - 1, m - 1, memo);
+    if (!yaCalculado){
+        memo[n][m] = fiboVignaMemo(n, m-1, memo, yaCalculado) + fiboVignaMemo(n - 1, m, memo, yaCalculado) + fiboVignaMemo(n - 1, m - 1, memo, yaCalculado);
+        yaCalculado[n][m] = true;
     }
     return memo[n][m];
 }
 
 long fiboVigna(int n, int m){
     long ** memo = new long * [n + 1];
+    bool ** yaCalculado = new bool * [n + 1];
     for(int i = 0; i < n + 1; i++){
         memo[i] = new long [m + 1]();
+        yaCalculado[i] = new bool [m + 1] ();
         for(int j = 0; j < m + 1; j++){
-            memo[i][j] = -1;
+            yaCalculado[i][j] = false;
         }
     }
-    return fiboVignaMemo(n, m, memo);
+    return fiboVignaMemo(n, m, memo, yaCalculado);
 }
 
 int main () {
