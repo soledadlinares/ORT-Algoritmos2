82987225c680b2b9f705ff05bbad412514869721
soledadlinares <sollinar@gmail.com>
2023-01-08 17:07:04 -0300

    problema del cambio PD guardando monedas

---
+++ MOCHILA/mochilaPD.cpp
@@ -0,0 +1,16 @@
+#include <climits>
+#include <string>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+
+int main (){
+
+
+
+    return 0;
+}
--- PROGRAMACION_DINAMICA/cambio.cpp
+++ PROGRAMACION_DINAMICA/cambio.cpp
@@ -24,10 +24,60 @@ int problemaDelCambio(int resto){
     return res[resto];
 }
 
+void problemaDelCambioGuardandoMonedas(int resto){
+    int * res = new int [resto+1]();
+    int * use = new int [resto+1]();
+    int monedas [] = {1, 2, 5, 10, 50};
+    res[0] = 0;
+    use[0] = 0;
+    for (int i = 1; i <= resto; i++){
+        int menor = res[i - 1]; //Uso moneda de 1
+        int monedaUsada = 1;
+        if (i >= 2){
+            if (menor >= res[i - 2]){
+                menor = res[i - 2];
+                monedaUsada = 2;
+            }
+        } 
+        if (i >= 5){
+            if (menor >= res[i - 5]){
+                menor = res[i - 5];
+                monedaUsada = 5;
+            }
+        }
+        if (i >= 10){
+            if (menor >= res[i - 10]){
+                menor = res[i - 10];
+                monedaUsada = 10;
+            }
+        }
+        if (i >= 50){
+            if (menor >= res[i - 50]){
+                menor = res[i - 50];
+                monedaUsada = 50;
+            }
+        };
+        res[i] = 1 + menor;
+        use[i] = monedaUsada;
+    }
+    cout << "La cantidad minima de monedas es " << res[resto] << endl;
+    int index = resto;
+    while (index != 0)
+    {
+        cout << "Usé la moneda de " << use[index] << endl;
+        index -= use[index];
+    }
+    
+}
+
 
 
 int main (){
+    cout << "Introduzca un monto: ";
+    int n;
+    cin >> n;
 
+    problemaDelCambioGuardandoMonedas(n);
 
 
     return 0;
