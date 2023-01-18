2dcd9f8cee941f3b945f17c940c4cf7e9fddf546
soledadlinares <sollinar@gmail.com>
2023-01-18 02:58:54 -0300

    implementaciones grafos matriz y lista adyacencia

--- GRAFOS/listaAdyacencia.cpp
+++ GRAFOS/listaAdyacencia.cpp
@@ -6,11 +6,76 @@ using namespace std;
 
 template <class T>
 
+struct NodoLista{
+    T dato;
+    NodoLista * sig;
+    NodoLista() : dato(NULL), sig(NULL) {};
+};
 
+struct Arista{
+    int origen;
+    int destino;
+    int peso;
+    Arista() : origen(0), destino(0), peso(0) {};
+};
 
-int main (){
+typedef NodoLista<Arista> * ListaAristas;
 
+class Grafo{
+    private:
+        ListaAristas * listaAdyacencia;
+        int V;
+        int A;
+        bool esDirigido;
+        bool esPonderado;
+    
+    public:
+        Grafo(int cant_V, bool es_dirigido, bool es_ponderado){
+            this->V = cant_V;
+            this->A = 0;
+            this->esDirigido = es_dirigido;
+            this->esPonderado = es_ponderado;
+            listaAdyacencia = new ListaAristas [V + 1];
+            for (int i = 1; i <= V; i++){
+                listaAdyacencia[i] = NULL;
+            }
+        }
 
+        void agregarArista(int origen, int destino, int peso){
+            int pesoArista = this->esPonderado ? peso : 1;
+            Arista * nueva = new Arista ();
+            nueva->origen = origen;
+            nueva->destino = destino;
+            nueva->peso = pesoArista;
 
-    return 0;
-}
+            NodoLista * nodo = new NodoLista<Arista>();
+            nodo->dato = nueva;
+            nodo->sig = listaAdyacencia[origen];
+            listaAdyacencia[origen] = nueva;
+            this->A++;
+
+            if(!esDirigido){
+                Arista * simetrica = new Arista();
+                simetrica->origen = destino;
+                simetrica->destino = origen;
+                simetrica->peso = pesoArista;
+
+                NodoLista * simetrico = new NodoLista<Arista>();
+                simetrico->dato = simetrica;
+                simetrico->sig = listaAdyacencia[destino];
+                listaAdyacencia[destino] = simetrico;
+            }
+        }
+
+        ListaAristas adyacentesA(int vertice){
+            ListaAristas retorno = NULL;
+            ListaAristas aux = listaAdyacencia[vertice];
+            while(aux != NULL){
+                Arista arista_aux = aux->dato;
+                retorno = new NodoLista<Arista>();
+                retorno->dato = arista_aux;
+                retorno->sig = retorno;
+            }
+            return retorno;
+        }
+};
--- GRAFOS/matrizAdyacencia.cpp
+++ GRAFOS/matrizAdyacencia.cpp
@@ -6,12 +6,63 @@ using namespace std;
 
 template <class T>
 
+struct NodoLista{
+    T dato;
+    NodoLista * sig;
+    NodoLista() : dato(NULL), sig(NULL) {};
+};
 
+struct Arista{
+    int origen;
+    int destino;
+    int peso;
+    Arista() : origen(0), destino(0), peso(0) {};
+};
 
+typedef NodoLista<Arista> * ListaAristas;
 
-int main (){
+class Grafo{
+    private:
+        int ** matriz;
+        int V;
+        int A;
+        int esDirigido;
+        int esPonderado;
+    public:
+        Grafo(int cant_vertices, bool es_dirigido, bool es_ponderado){
+            this->V = cant_vertices;
+            this->A = 0;
+            this->esDirigido = es_dirigido;
+            this->esPonderado = es_ponderado;
+            this->matriz = new int * [V + 1]();
+            for(int i = 1; i <= V; i++){
+                this->matriz[i] = new int [V + 1]();
+                for (int j = 1; j <= V; j++){
+                    this->matriz[i][j] = INF;
+                }
+            }
+        }
 
+        void agregarArista(int origen, int destino, int peso){
+            this->matriz[origen][destino] = esPonderado ? peso : 1;
+            if (!esDirigido) this->matriz[destino][origen] = esPonderado ? peso : 1;
+            this->A++;
+        }
 
-
-    return 0;
-}
+        ListaAristas adyacentesA(int vertice){
+            int inf = INF;
+            ListaAristas * retorno = NULL;
+            for(int i = 1; i <= this->V; i++){
+                if(this->matriz[vertice][i] != INF){
+                    Arista * aux = new Arista();
+                    aux->origen = vertice;
+                    aux->destino = i;
+                    aux->peso = matriz[vertice][i];
+                    retorno = new NodoLista<Arista>();
+                    retorno->dato = aux;
+                    retorno->sig = retorno;
+                }
+            }
+            return retorno;
+        }
+};
