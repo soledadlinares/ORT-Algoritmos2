e9a193edb658fac23d3405c8acbaedd6156e0e51
soledadlinares <sollinar@gmail.com>
2023-01-31 00:10:40 -0300

    arranque problema de sudoku 2/22

---
+++ EXAMENES/2022_02_EJ2.cpp
@@ -0,0 +1,27 @@
+#include <climits>
+#include <string>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+    EJERCICIO 2:
+    Resolver sudoku mediante la técnica de Backtracking.
+    - Se recibe un tablero incompleto
+    - Se retorna la solución.
+
+    NOTAS:
+    - Puede considerar que siempre hay solucion
+    - El tablero tiene 0s en las casillas vacias.
+    - Los tableros son 9x9
+
+    
+*/
+
+
+
+
+int main (){
+
+    return 0;
+}
